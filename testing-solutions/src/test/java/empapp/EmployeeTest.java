fd06f96c67eae1fa0fdecc4179426d0b2fe3f350
NagyLP <nagy.peter@pr.hu>
2022-04-16 16:03:29 +0200

    Szerkezete,@Before*,Disable,Order

---
+++ testing-solutions/src/test/java/empapp/EmployeeTest.java
@@ -0,0 +1,55 @@
+package empapp;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+public class EmployeeTest {
+
+    Employee employee;
+
+    public EmployeeTest() {
+        System.out.println("\nCONSTRUCTOR");
+    }
+
+    @BeforeAll
+    static  void beforeAll(){
+        System.out.println("\nBEFORE ALL");
+    }
+
+    @BeforeEach
+    void initEmployee() {
+        System.out.println("\nINIT");
+        employee = new Employee("John Doe", 1970);
+    }
+
+
+    @Test
+//    @Order(2)
+//    @Disabled("Until fix Projektmunka Rendelés")
+//    @DisabledOnOs(OS.WINDOWS)
+    void testGetAgeWithZero() {
+        System.out.println("TC2");
+        System.out.println(employee);
+        employee.setName("Jack Doe");
+        assertEquals(0,
+                new Employee("John Doe", 1970)
+                        .getAge(1970));
+    }
+
+    @Test
+//    @Order(1)
+    void testGetAge() {
+        System.out.println("TC1");
+        System.out.println(employee);
+        assertEquals(52,
+                new Employee("John Doe", 1970)
+                        .getAge(2022));
+    }
+
+
+}

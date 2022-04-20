7282878e822448cc09fa439c0477214b671a63a3
NagyLP <nagy.peter@pr.hu>
2022-04-20 18:45:05 +0200

    TRANZAKCIÓkezelés-Kieg

--- labs-solutions/src/main/java/elearning/Main.java
+++ labs-solutions/src/main/java/elearning/Main.java
@@ -3,8 +3,11 @@ package elearning;
 import org.flywaydb.database.mysql.mariadb.MariaDBDatabase;
 import org.mariadb.jdbc.MariaDbDataSource;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Main {
 
@@ -34,6 +37,49 @@ public class Main {
         } catch (SQLException sqle) {
 
         }
-
     }
+
+// KIEG. Pl.: StatementFactory
+//    public static Connection continuousConnection(DataSource dataSource) throws SQLException {
+//        @SuppressWarnings("java:S2095")
+//        Connection connection = dataSource.getConnection();
+//        connection.setAutoCommit(false);
+//        return connection;
+//    }
+//
+//    public static PreparedStatement getParameterizedStatement
+//            (Connection connection, String stringSql, Object... values)
+//            throws SQLException {
+//        PreparedStatement stmt = connection.prepareStatement(stringSql);
+//        for (int i = 1; i <= values.length; i++) {
+//            Object parameters = values[i - 1];
+//
+//            if (parameters instanceof String s) {
+//                stmt.setString(i, s);
+//
+//            } else if (parameters instanceof Integer j) {
+//                stmt.setInt(i, j);
+//
+//            } else if (parameters instanceof Long l) {
+//                stmt.setLong(i, l);
+//
+//            } else if (parameters instanceof Double d) {
+//                stmt.setDouble(i, d);
+//
+//            } else if (parameters instanceof LocalDate date) {
+//                stmt.setDate(i, Date.valueOf(date));
+//
+//            } else if (parameters instanceof LocalDateTime dateTime) {
+//                stmt.setTimestamp(i, Timestamp.valueOf(dateTime));
+//
+//            } else if (parameters instanceof LocalTime time) {
+//                stmt.setTime(i, Time.valueOf(time));
+//
+//            } else {
+//                throw new IllegalArgumentException(String.format("Invalid parameter type at SQL-index %d: %s",
+//                        i, parameters.getClass().getName()));
+//            }
+//        }
+//        return stmt;
+//    }
 }

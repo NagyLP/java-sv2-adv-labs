82aa591010c7c4a28424304f43e7247eee0a3ae1
NagyLP <nagy.peter@pr.hu>
2022-06-15 20:15:48 +0200

    init

---
+++ java-sv2-adv-project-NagyLaszloPeter/src/main/java/PostEr/JavaSv2AdvProjectNagyLaszloPeterApplication.java
@@ -0,0 +1,13 @@
+package PostEr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSv2AdvProjectNagyLaszloPeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSv2AdvProjectNagyLaszloPeterApplication.class, args);
+	}
+
+}

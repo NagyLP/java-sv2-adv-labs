c9829c8ed24a010a7fe2e73f87fee542c1535b9a
NagyLP <nagy.peter@pr.hu>
2022-10-03 20:18:00 +0200

    init

---
+++ labs-solutions/consultations/incrementer/src/test/java/incrementer/IncrementerApplicationTests.java
@@ -0,0 +1,13 @@
+package incrementer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncrementerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-solutions/src/main/java/locations/main.java
@@ -0,0 +1,8 @@
+package locations;
+
+public class main {
+
+    public static void main(String[] args) {
+
+    }
+}
